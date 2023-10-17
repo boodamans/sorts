fe8ba2e8115487bf8296fea2a0d58b0d4650bec0
Sam Narciso <109814056+boodamans@users.noreply.github.com>
2023-10-17 13:38:13 -0400

    Add files via upload

---
+++ bubble.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+    const len = arr.length;
+  
+    for (let i = 0; i < len - 1; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          // Swap the elements if they are in the wrong order
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  
+    return arr;
+  }
+
+module.exports = bubbleSort;
